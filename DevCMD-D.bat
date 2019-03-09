9e5e1bbeb26fb7654bd6d39d0aaff4bdaf503aaa
huhongjun <13114837@qq.com>
2019-03-09 21:21:27 +0800

    本地编译相关文件

---
+++ DevCMD-D.bat
@@ -0,0 +1,16 @@
+
+@set AD_HOME=D:\ADeveloper
+@set PATH=%AD_HOME%
+
+@rem ----------- jdk1.5.0 jdk1.5.0 jdk1.5.0 ------------
+@set JAVA_HOME=%AD_HOME%\java\jdk1.8.0_77x64
+@set PATH=%JAVA_HOME%\bin;%PATH%
+
+@rem ----------- maven-2.0 maven-2.0 maven-2.0-----------
+@set M2_HOME=%AD_HOME%\apache-maven\maven-3.5.4
+@set M2=%AD_HOME%\apache-maven\maven-3.5.4\bin
+@set PATH=%AD_HOME%\apache-maven\maven-3.5.4\bin;%PATH%
+
+
+@%COMSPEC%
+
---
+++ maven-lib/mvn-install-elasticsearch-sql.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=org.nlpcn -DartifactId=elasticsearch-sql -Dversion=5.3.2.0 -Dpackaging=jar -DgeneratePom=true -Dfile=elasticsearch-sql-5.3.2.0.jar
